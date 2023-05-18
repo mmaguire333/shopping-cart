04c842bebd83d2f73cd1659be7e0f219c3c19000
Matt <mmaguire333@gmail.com>
2023-05-17 20:23:13 -0600

    Add Navbar and routes to Home, Shop, and Cart components in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Shop from './Components/Shop';
+import Cart from './Components/Cart';
+import Navbar from './Components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
