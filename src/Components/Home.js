821de62f2d46027278f464caec1b6ef10b2c45c9
Matt <mmaguire333@gmail.com>
2023-05-24 18:06:29 -0600

    Change carousel image caption font size

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -21,19 +21,19 @@ const Home = () => {
             <Carousel autoPlay={true} interval={5000} infiniteLoop={true} showArrows={false} showThumbs={false}>
                 <div id="slide-one">
                     <img src={firstImage} alt=""/>
-                    <p><a href="https://www.freepik.com/free-photo/fashionable-woman-great-mood-posing-wooden-wall_12858216.htm#query=winter%20jackets&position=1&from_view=search&track=ais">Image by lookstudio</a> on Freepik</p>
+                    <p className="image-credit"><a href="https://www.freepik.com/free-photo/fashionable-woman-great-mood-posing-wooden-wall_12858216.htm#query=winter%20jackets&position=1&from_view=search&track=ais">Image by lookstudio</a> on Freepik</p>
                 </div>
                 <div id="slide-two">
                     <img src={secondImage} alt=""/>
-                    <p><a href="https://www.freepik.com/free-photo/portrait-bearded-indian-male-dressed-winter-jacket-grey-background_26399842.htm#query=winter%20jackets&position=42&from_view=search&track=ais#position=42&query=winter%20jackets">Image by fxquadro</a> on Freepik</p>
+                    <p className="image-credit"><a href="https://www.freepik.com/free-photo/portrait-bearded-indian-male-dressed-winter-jacket-grey-background_26399842.htm#query=winter%20jackets&position=42&from_view=search&track=ais#position=42&query=winter%20jackets">Image by fxquadro</a> on Freepik</p>
                 </div>
                 <div id="slide-three">
                     <img src={thirdImage} alt=""/>
-                    <p><a href="https://www.freepik.com/free-photo/portrait-bearded-male-dressed-warm-jacket-scarf-hold-backpack-isolated-grey-background_26472358.htm#query=winter%20jackets&position=15&from_view=search&track=ais">Image by fxquadro</a> on Freepik</p>
+                    <p className="image-credit"><a href="https://www.freepik.com/free-photo/portrait-bearded-male-dressed-warm-jacket-scarf-hold-backpack-isolated-grey-background_26472358.htm#query=winter%20jackets&position=15&from_view=search&track=ais">Image by fxquadro</a> on Freepik</p>
                 </div>
                 <div id="slide-four">
                     <img src={fourthImage} alt=""/>
-                    <p><a href="https://www.freepik.com/free-photo/romantic-european-girl-pink-coat-smiling-cold-day-cheerful-fair-haired-woman-having-fun-during-winter-photoshoot_11208403.htm#query=winter%20jackets&position=40&from_view=search&track=ais">Image by lookstudio</a> on Freepik</p>
+                    <p className="image-credit"><a href="https://www.freepik.com/free-photo/romantic-european-girl-pink-coat-smiling-cold-day-cheerful-fair-haired-woman-having-fun-during-winter-photoshoot_11208403.htm#query=winter%20jackets&position=40&from_view=search&track=ais">Image by lookstudio</a> on Freepik</p>
                 </div>
             </Carousel>
         </div>
