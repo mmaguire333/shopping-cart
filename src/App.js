334dae0af89ffe7a5c8628dfcc0e82aa55e7c530
Matt <mmaguire333@gmail.com>
2023-05-24 18:10:55 -0600

    Create a cartItems state array and addToCart function to update state passing them as props to Cart and Shop respectively

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Shop from './Components/Shop';
@@ -7,14 +8,34 @@ import Navbar from './Components/Navbar';
 import './App.css';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (name, price, quantity) => {
+    let newItems = []
+    let index = -1;
+
+    for(let i = 0; i < cartItems.length; i++) {
+      if(cartItems[i].name === name && cartItems[i].price === price) {
+        index = i;
+      }
+    }
+
+    if(index === -1) {
+      setCartItems([...cartItems, {name: name, price: price, quantity: quantity}]);
+    } else {
+      newItems = [...cartItems.slice(0, index), {name: name, price: price, quantity: cartItems[index].quantity + quantity}, ...cartItems.slice(index + 1)];
+      setCartItems(newItems);
+    }
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar></Navbar>
+        <Navbar itemCount={cartItems.map((item => item.quantity)).reduce((accumulator, currentValue) => accumulator + currentValue, 0)}></Navbar>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route path="/shop" element={<Shop addToCart={addToCart} itemCount={cartItems.map((item => item.quantity)).reduce((accumulator, currentValue) => accumulator + currentValue, 0)} />} />
+          <Route path="/cart" element={<Cart cartItems={cartItems}/>} />
         </Routes>
       </BrowserRouter>
     </div>
